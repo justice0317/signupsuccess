948b4e5817ad96be2a68e5dce6c68514de9a4f66
陳政邑 <chenzhengyi@Justices-MacBook-Air.local>
2024-11-20 10:50:09 +0800

    First commit of my website

---
+++ script.js
@@ -0,0 +1,9 @@
+// 按鈕功能：導向登入頁
+function redirectToLogin() {
+    window.location.href = "https://login-phi-amber.vercel.app"; // 替換為您的登入頁面網址
+}
+
+// 按鈕功能：導向主頁
+function redirectToHome() {
+    window.location.href = "https://home-rose-rho.vercel.app"; // 替換為您的主頁網址
+}
